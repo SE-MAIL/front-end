c13bffcc21e84a29bbe898926411f6ad62d4d78f
gobin2 <bcgo9907@gmail.com>
2021-11-17 21:56:51 +0900

    starter code

---
+++ app/components/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Home() {
+    return(
+        <View>
+            <Text>
+            hello
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
