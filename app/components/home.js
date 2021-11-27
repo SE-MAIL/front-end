3a2a038785829df74094b166d7e19db0a79487b2
memoday <samyci@naver.com>
2021-11-27 22:03:04 +0900

    시작화면추가

--- app/components/home.js
+++ app/components/home.js
@@ -1,14 +1,49 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
-function Home() {
-    return(
-        <View>
-            <Text>
-            hello
-            </Text>
-        </View>
-    )
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logoSub}>
+        모르는 사이에 조금씩 조금씩
+      </Text>
+      <Text style={styles.logo}>
+        시나브로
+      </Text>
+      <Text style={styles.logoSub2}>
+        Cinabro
+      </Text>
+    </View>
+  );
 }
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#000000',
+    padding: 8,
+  },
+  logoSub: {
+    margin: -30,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color:'#73bf69',
+  },
+  logo: {
+    margin: 24,
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+  logoSub2: {
+    margin: -50,
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#888181',
+  },
+  
+});
