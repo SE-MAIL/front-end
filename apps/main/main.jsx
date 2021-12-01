2ee9ee36646c05b3c1cd23c8e961736b1bfd4129
memoday <samyci@naver.com>
2021-12-02 07:20:10 +0900

    ++asset

--- apps/main/main.jsx
+++ apps/main/main.jsx
@@ -8,6 +8,15 @@ export default function Home() {
       <Text style={styles.logo}>
         안녕하세요, 사용자님
       </Text>
+      <Text style={{textAlign:'center', fontSize: 40, opacity: 0.4, color:'#16CA1C', marginBottom: -15,fontWeight:'bold',}}>
+        C02
+      </Text>
+      <Text style={{textAlign:'center', fontSize: 70,fontWeight:'bold', color:'#16CA1C'}}>
+        120kg
+      </Text>
+      <Text style={{textAlign:'center', fontSize: 20,fontWeight:'bold', color:'#dbe1db', opacity: 0.5, marginTop: -10}}>
+        평균 배출량: 200kg
+      </Text>
       <Text style={styles.textUnder}>
         월간 C02 배출량
       </Text>
@@ -18,7 +27,6 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     backgroundColor: '#000000',
     padding: 8,
   },
@@ -30,15 +38,19 @@ const styles = StyleSheet.create({
     color:'#73bf69',
   },
   globe: {
-    width: '50%',
+    width: '20%',
     height: '20%',
+    marginLeft: 10,
     justifyContent: 'center',
     alignItems: 'center',
     resizeMode: 'contain',
+    marginBottom: -100,
+    opacity: 0.2,
+    transform: [{rotate: '-23deg'}],
   },
   logo: {
-    margin: 24,
-    fontSize: 25,
+    margin: 30,
+    fontSize: 31,
     fontWeight: 'bold',
     color: 'white',
   },
