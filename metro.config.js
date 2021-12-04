d97aa707208d4c6a76893ec92a99227b6245d5ee
gobin2 <bcgo9907@gmail.com>
2021-12-04 14:42:05 +0900

    feat/ 네비게이션 환경 설정

--- metro.config.js
+++ metro.config.js
@@ -8,7 +8,7 @@
 module.exports = {
   resolver: {
     /* resolver options */
-    sourceExts: ['jsx', 'js'], //add here
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx'], //add here
   },
   transformer: {
     getTransformOptions: async () => ({
